aaa4e9d89675a708839097917197ae71b63043db
ldfxxx404 <limur9990@gmail.com>
2025-06-13 12:54:17 +0300

    feat: add input field

--- QR-maker/src/app/page.tsx
+++ QR-maker/src/app/page.tsx
@@ -1,11 +1,15 @@
-import { QRCodeSVG } from "qrcode.react"
+import { QRCodeSVG } from "qrcode.react";
 
 export default function Main() {
-    return (
-        <div className="flex">
-            <div className="mx-auto my-auto mt-96">
-                <QRCodeSVG value="https://github.com/ldfxxx404"></QRCodeSVG>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex justify-center">
+      <div className="mt-64 flex flex-col items-center gap-10">
+        <QRCodeSVG value="https://github.com/ldfxxx404" size={200}></QRCodeSVG>
+        <input  
+        type="text" 
+        placeholder="Input your text"
+        className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+      </div>
+    </div>
+  );
 }
